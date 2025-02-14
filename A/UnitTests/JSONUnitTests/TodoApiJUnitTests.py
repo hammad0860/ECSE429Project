3000af6725f49094b55cfd7b9967ad596e550129
hammad0860 <muhammad.h0860@gmail.com>
2025-02-13 21:13:29 -0500

    Update TodoApiJUnitTests.py

--- A/UnitTests/JSONUnitTests/TodoApiJUnitTests.py
+++ A/UnitTests/JSONUnitTests/TodoApiJUnitTests.py
@@ -21,26 +21,7 @@ class TodoApiUnitTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        global APPLICATION_RUNNING
-        global APPLICATION_PROCESS
-        if not APPLICATION_RUNNING:
-            try:
-                APPLICATION_PROCESS = subprocess.Popen(
-                    ["java", "-jar", os.path.join(WORKING_DIR, "runTodoManagerRestAPI-1.5.5.jar")],
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL,
-                    cwd=WORKING_DIR,
-                    shell=True
-                )
-                cls.wait_for_server()    
-                APPLICATION_RUNNING = True
-            except Exception as e:
-                APPLICATION_RUNNING = False
-                print(f"Error starting the application: {e}")
-        
-        if not APPLICATION_RUNNING:
-            raise RuntimeError("The application could not be started and the unit tests have been terminated.")
-        
+
         cls.expected_json_all_ids = {
                 "todos": [
                     {
@@ -64,10 +45,69 @@ class TodoApiUnitTests(unittest.TestCase):
                     }
                 ]
             }
+        
+        cls.dishes_json = {
+                    
+                        "title": "wash dishes",
+                        "doneStatus": False,
+                        "description": "",
+                        
+        }
+        cls.dishes_json_response = {
+                        "id": "3",
+                        "title": "wash dishes",
+                        "doneStatus": "false",
+                        "description": ""
+        }
 
+        cls.update_paperwork_json = {
+                "title": "scan paperwork",            
+                "doneStatus": True,
+                "description" : "all paperwork has been scanned"
 
-    @classmethod
-    def tearDownClass(cls):
+        }
+        
+        cls.update_paperwork_json_response = {
+                        "id": "1",
+                        "title": "scan paperwork",
+                        "doneStatus": "true",
+                        "description": "all paperwork has been scanned",
+                        "categories": [{"id": "1"}],
+                        "tasksof": [{"id": "1"}]
+        }
+
+        cls.update_paperwork_json_response_put = {
+                        "id": "1",
+                        "title": "scan paperwork",
+                        "doneStatus": "true",
+                        "description": "all paperwork has been scanned"
+        }   
+            
+            
+    def setUp(self):
+        global APPLICATION_RUNNING
+        global APPLICATION_PROCESS
+        if not APPLICATION_RUNNING:
+            try:
+                APPLICATION_PROCESS = subprocess.Popen(
+                    ["java", "-jar", os.path.join(WORKING_DIR, "runTodoManagerRestAPI-1.5.5.jar")],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                    cwd=WORKING_DIR,
+                    shell=True
+                )
+                self.wait_for_server()    
+                APPLICATION_RUNNING = True
+            except Exception as e:
+                APPLICATION_RUNNING = False
+                print(f"Error starting the application: {e}")
+        
+        if not APPLICATION_RUNNING:
+            raise RuntimeError("The application could not be started and the unit tests have been terminated.")
+
+        
+    def tearDown(self):
+        global APPLICATION_RUNNING
         global APPLICATION_PROCESS
         try:
             requests.get(f"{BASE_URL}/shutdown")
@@ -77,9 +117,10 @@ class TodoApiUnitTests(unittest.TestCase):
         APPLICATION_PROCESS.terminate()  
         APPLICATION_PROCESS.wait()
         APPLICATION_PROCESS = None
+        APPLICATION_RUNNING = False
 
 
-
+    # Tests get all todos
     def test_get_todos(self):
         response = requests.get(BASE_URL + "/todos", headers={"Accept": "application/json"})
         self.assertEqual(response.status_code, 200)
@@ -89,6 +130,7 @@ class TodoApiUnitTests(unittest.TestCase):
 
 
 
+    # Tests get todo by id
     def test_get_todo(self):
         id = 1
         response = requests.get(BASE_URL + f"/todos/{id}", headers={"Accept": "application/json"})
@@ -98,18 +140,86 @@ class TodoApiUnitTests(unittest.TestCase):
         self.assertEqual(diff, {}, f"Differences found: {diff}")
 
 
+
+    # Tests get header for all todos
+    def test_get_header(self):
+        response = requests.head(BASE_URL + f"/todos", headers={"Accept": "application/json"})
+        self.assertEqual(response.status_code, 200)
+
+
+    # Tests get header for todo by id
+    def test_get_header_id(self):
+        id = 1
+        response = requests.head(BASE_URL + f"/todos/{id}", headers={"Accept": "application/json"})
+        self.assertEqual(response.status_code, 200)
+
+
+
+    # Tests post todo
+    def test_post_todo(self):
+        id = 3
+        response = requests.post(BASE_URL + f"/todos", headers={"Accept": "application/json"}, json=self.dishes_json)
+        self.assertEqual(response.status_code, 201)
+        
+        response = requests.get(BASE_URL + f"/todos/{id}", headers={"Accept": "application/json"})
+        self.assertEqual(response.status_code, 200)
+        actual_json = response.json()
+        diff = DeepDiff(actual_json["todos"][0], self.dishes_json_response, ignore_order=True)
+        self.assertEqual(diff, {}, f"Differences found: {diff}")
+
+
+    # Tests post todo by id
+    def test_post_todo_by_id(self):
+        id = 1
+        response = requests.post(BASE_URL + f"/todos/{id}", headers={"Accept": "application/json"}, json=self.update_paperwork_json)
+        self.assertEqual(response.status_code, 200)
+        
+        response = requests.get(BASE_URL + f"/todos/{id}", headers={"Accept": "application/json"})
+        self.assertEqual(response.status_code, 200)
+        actual_json = response.json()
+        diff = DeepDiff(actual_json["todos"][0], self.update_paperwork_json_response, ignore_order=True)
+        self.assertEqual(diff, {}, f"Differences found: {diff}")
+
+
+
+    # Tests put todo by id
+    def test_put_todo_by_id(self):
+        id = 1
+        response = requests.put(BASE_URL + f"/todos/{id}", headers={"Accept": "application/json"}, json=self.update_paperwork_json)
+        self.assertEqual(response.status_code, 200)
+        
+        response = requests.get(BASE_URL + f"/todos/{id}", headers={"Accept": "application/json"})
+        self.assertEqual(response.status_code, 200)
+        actual_json = response.json()
+        diff = DeepDiff(actual_json["todos"][0], self.update_paperwork_json_response_put, ignore_order=True)
+        self.assertEqual(diff, {}, f"Differences found: {diff}")
+
+    # Tests delete todo by id
+    def test_delete_todo_by_id(self):
+        id = 2
+        response = requests.delete(BASE_URL + f"/todos/{id}", headers={"Accept": "application/json"})
+        self.assertEqual(response.status_code, 200)
+        
+        response = requests.get(BASE_URL + f"/todos/{id}", headers={"Accept": "application/json"})
+        self.assertEqual(response.status_code, 404)
+
+        expected_error = {"errorMessages": [f"Could not find an instance with todos/{id}"]}
+        actual_json = response.json()  
+        self.assertEqual(actual_json, expected_error)
+
+
     
+
 
 
 
 
     @staticmethod
     def wait_for_server():
-        while True:
-            response = requests.get(f"{BASE_URL}/todos", timeout=3)
-            if response.status_code == 200:
-                return 
-            time.sleep(2)  
+            while True:
+                response = requests.get(f"{BASE_URL}/todos", timeout=3)
+                if response.status_code == 200:
+                    return 
 
 
 
